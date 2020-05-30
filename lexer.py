8377f9a24305b1b1b280fef82d81e6943d34804a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-05-30 15:47:23 +0300

    lexer: Add simple lexer based on matching const strings or regexps.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

---
+++ lexer.py
@@ -0,0 +1,61 @@
+import re
+
+
+class LexerError(Exception):
+
+    def __init__(self, msg, context):
+        self.msg = msg
+        self.ctx = context
+
+
+class Lexer:
+
+    def __init__(self, l=None):
+        self.l = l
+        self._ws = re.compile("[ \t]+")
+
+    def init(self, l):
+        self.l = l
+
+    def error(self, msg, ctx=None):
+        if ctx is None:
+            ctx = self.l
+        raise LexerError(msg, ctx)
+
+    def eol(self):
+        return not self.l
+
+    def skipws(self):
+        m = self._ws.match(self.l)
+        if m:
+            self.l = self.l[m.end():]
+
+    def check(self, s):
+        return self.l.startswith(s)
+
+    def match(self, s, skipws=True):
+        if self.check(s):
+            self.l = self.l[len(s):]
+            if skipws:
+                self.skipws()
+            return True
+
+    def match_re(self, r, skipws=True):
+        m = r.match(self.l)
+        if m:
+            self.l = self.l[m.end():]
+            if skipws:
+                self.skipws()
+            return m.group()
+
+    def expect(self, s, skipws=True):
+        res = self.match(s, skipws)
+        if res:
+            return res
+        self.error("Expected %r" % s)
+
+    def expect_re(self, r, skipws=True):
+        res = self.match_re(r, skipws)
+        if res:
+            return res
+        self.error("Expected %r" % r)
