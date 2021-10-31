8c42d321df63642d1f3541795d297a70aa8b2bf5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-10-31 21:40:28 +0300

    config: Add SPLIT_BB_AFTER_CALL option.
    If it's True (default), a basic block is split after a "call"
    instruction, which was previous unconditional behavior. If set
    to False, splitting doesn't happen. This is useful e.g. when
    interfacing with other ILs, e.g. LLVMIR.
    
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

--- pseudoc/config.py
+++ pseudoc/config.py
@@ -1,1 +1,6 @@
 # Config options.
+
+# Split basic block after a call instructions. Useful for tracking
+# which variables are live after call (which then can be assigned
+# to callee-save registers).
+SPLIT_BB_AFTER_CALL = True
